940a648605279a9de61e8099d5edd62f358877dd
OSHO <oshomani2006@gmail.com>
2026-04-10 20:25:52 +0530

    fix: bump service worker cache to v3 to force new UI update

--- public/sw.js
+++ public/sw.js
@@ -1,4 +1,4 @@
-const CACHE_NAME = 'aihub-v2';
+const CACHE_NAME = 'aihub-v3';
 const STATIC_ASSETS = ['/', '/index.html', '/style.css', '/app.js', '/manifest.json'];
 
 self.addEventListener('install', (event) => {
